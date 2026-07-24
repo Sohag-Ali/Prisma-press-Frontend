6d75e362fd98155e953959fc14a7552ab04b2f3d
sohag142 <sohag2879@gmail.com>
2026-07-24 11:12:24 +0600

    user login and un authorize user based route protest

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,52 @@
+
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/news"];
  
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
-    console.log("pathname", pathname);
-    console.log("request", request.nextUrl);
-    console.log("proxy");
+
+    // const cookieStore = await cookies();
+    // const accessToken = cookieStore.get("accessToken")?.value || null;
+
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+
+    let userRole = null;
+
+    if(decodedToken){
+        userRole = decodedToken.role;
+    }
+
+    if(accessToken && AUTH_ROUTES.includes(pathname)){
+        if(userRole === "USER"){
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        }
+        else if(userRole === "AUTHOR"){
+            return NextResponse.redirect(new URL('/author-dashboard', request.url));
+        }
+        else if(userRole === "ADMIN"){
+            return NextResponse.redirect(new URL('/admin-dashboard', request.url));
+        }
+        else{
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+    }
+
+    const isPublic = PUBLIC_ROUTES.some(route => pathname === route || pathname.startsWith(route + '/'));
+
+    const isAuthRoute = AUTH_ROUTES.some(route => pathname === route || pathname.startsWith(route + '/'));
+
+    if(!accessToken && !isPublic && !isAuthRoute){
+        return NextResponse.redirect(new URL('/login', request.url));
+
+    }
+
 //   return NextResponse.redirect(new URL('/', request.url))
   return NextResponse.next()
 }
